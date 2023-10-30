fac04faf46600a0c9b491579b6ad6f9347f666c3
erdalHulagu <erdalhulahu@gmail.com>
2023-10-30 21:20:23 +0000

    30/10/23

--- src/component/user/chat/message-card.jsx
+++ src/component/user/chat/message-card.jsx
@@ -16,7 +16,7 @@ const MessageCard = ({ isRegUserMessage, content, img }) => {
             <div className='py-2 px-2 max-w-[50%] max-h-[25%] '>
                 <img className='rounded' src="https://cdn.pixabay.com/photo/2016/05/02/13/17/deer-1367217_1280.jpg" alt="" />
             </div>
-            <div className={` flex justify-center h- w-full items-center max-w-[50%] py-3 px-2 rounded ${isRegUserMessage ? "self-start bg-slate-200 text-slate-800" : "self-end bg-slate-400 text-white"}`}>
+            <div className={` flex  h- w-full items-center max-w-[50%] py-3 px-2 rounded ${isRegUserMessage ? "self-start bg-slate-200 text-slate-800" : "self-end bg-slate-400 text-white"}`}>
 
                 <p className=' your-paragraph-styles ' >{content} adasdasasasasxasxasxaAAzAzazazAzAsx`sx`sx`sx`</p>
 

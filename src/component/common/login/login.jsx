47d6da09d0a93999d1cba39968b6e012ab78b4e0
erdalHulagu <erdalhulahu@gmail.com>
2024-05-06 20:59:48 +0100

    06/05/24

--- src/component/common/login/login.jsx
+++ src/component/common/login/login.jsx
@@ -5,29 +5,23 @@ import { Container, Form, Spinner } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { BiSolidLeftArrowSquare } from "react-icons/bi"
 import * as Yup from "yup";
-import axios from "axios";
-import { login } from "../../../service/user-service";
+import { getUser, login } from "../../../service/user-service";
+import { useDispatch, useSelector } from "react-redux";
+import { encryptedLocalStorage } from "../../../helper/auth-token/encrypt-storage";
+import { loginFailed, loginSucces } from "../../../redux/store/slices/auth/auth-slice";
+import { toast } from "../../../helper/swal";
 
 const Login = () => {
-    const navigater = useNavigate();
+    
     const [loading, setLoading] = useState(false)
-
-    const handleNavigater = () => {
-        navigater('/home');
-    }
-   
-
-    
-    const handleNavigate = () => {
-        navigater("/")
-    }
-    
-
+    const navigate = useNavigate();
+    const dispach = useSelector();
+    const selector = useDispatch();
 
     const initialValues = {
         email: "",
         password: "",
-      
+
     };
 
     const validationSchema = Yup.object({
@@ -40,15 +34,20 @@ const Login = () => {
     const onSubmit = async (values) => {
         setLoading(true);
         try {
-            const resp = login(values);
-            console.log(resp.data);
+            const respAuth = login(values);
+            console.log(respAuth.data);
             formik.resetForm();
-            localStorage.setItem("token", resp.data.token)
+            encryptedLocalStorage.setItem("token", respAuth.data.token)
+
+            const respUser=await getUser();
+            dispach(loginSucces(respUser.date));
+            navigate("/")
 
         } catch (err) {
-            console.log(err);
-            // alert(err.response.data.message);
-        }finally{
+            dispach(loginFailed());
+            toast(err.response.data.message, "error")
+            
+        } finally {
             setLoading(false);
         }
     };
@@ -60,55 +59,54 @@ const Login = () => {
     });
 
     return (
-        
-                        <Container className="mt-3 ">
-                            <Form noValidate onSubmit={formik.handleSubmit}>
 
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control
-                                        type="email"
-                                        placeholder="Enter email"
-                                        {...formik.getFieldProps("email")}
-                                        isInvalid={formik.touched.email && !!formik.errors.email}
-                                        isValid={formik.touched.email && !formik.errors.email}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {formik.errors.email}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                                <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder="Password"
-                                        {...formik.getFieldProps("password")}
-                                        isInvalid={formik.touched.password && !!formik.errors.password}
-                                        isValid={formik.touched.password && !formik.errors.password}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {formik.errors.password}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
+        <Container className="mt-3 ">
+            <Form noValidate onSubmit={formik.handleSubmit}>
 
-                                <div className="w-full h-14 flex items-center justify-end ">
-                                    <div>
-                                        <button className=" h-10  w-24 rounded-lg bg-slate-700 text-slate-100  hover:text-slate-950 hover:bg-gray-100   " 
-                                        type="submit"
-                                        //  onClick={handleNavigater}
-                                        disabled={!(formik.dirty && formik.isValid) || loading}
-                                        >
-                                            {loading && <Spinner animation="border" size="sm" />}
-                                        Login
-                                        </button>
-                                    </div>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        {...formik.getFieldProps("email")}
+                        isInvalid={formik.touched.email && !!formik.errors.email}
+                        isValid={formik.touched.email && !formik.errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.email}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        {...formik.getFieldProps("password")}
+                        isInvalid={formik.touched.password && !!formik.errors.password}
+                        isValid={formik.touched.password && !formik.errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.password}
+                    </Form.Control.Feedback>
+                </Form.Group>
 
-                                </div>
+                <div className="w-full h-14 flex items-center justify-end ">
+                    <div>
+                        <button className=" h-10  w-24 rounded-lg bg-slate-700 text-slate-100  hover:text-slate-950 hover:bg-gray-100   "
+                            type="submit"
+                            disabled={!(formik.dirty && formik.isValid) || loading}
+                        >
+                            {loading && <Spinner animation="border" size="sm" />}
+                            Login
+                        </button>
+                    </div>
 
-                            </Form>
-                        </Container>
+                </div>
 
-      
+            </Form>
+        </Container>
+
+
 
     );
 };

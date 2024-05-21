67c19a4bd689ec1d6e69e3dc95c77f1ac215c799
erdalHulagu <erdalhulahu@gmail.com>
2024-05-21 08:23:25 +0100

    21/05/24

---
+++ src/component/user/my-profile/update-user-profile.jsx
@@ -0,0 +1,161 @@
+// import axios from "axios";
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import { Container, Form, Spinner } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { BiSolidLeftArrowSquare } from "react-icons/bi"
+import * as Yup from "yup";
+import { getUser, login, updateUser } from "../../../service/user-service";
+import { useDispatch, useSelector } from "react-redux";
+import { encryptedLocalStorage } from "../../../helper/auth-token/encrypt-storage";
+import { toast } from "../../../helper/swal";
+import { useAppDispatch } from "../../../redux/store/hooks";
+import { loginFailed, loginSuccess } from "../../../redux/store/slices/auth-slice";
+
+const UpdateUserProfile = () => {
+    
+    const navigate= useNavigate();
+    const [loading, setLoading] = useState(false);
+    const initialValues = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        phoneNumber: "",
+        address: "",
+        remember: false,
+    };
+
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .email("Type in a valid email adress")
+            .required("Don't leave blank"),
+        password: Yup.string().required("Type in password"),
+    });
+
+    const onSubmit = async (values) => {
+        setLoading(true);
+        try {
+            const resp=await updateUser(values);
+           toast("You're registered", "success");
+            formik.resetForm();
+            console.log(resp.data);
+            navigate('/login')
+
+        } catch (err) {
+            toast(err.response.data.messsage,"error")
+            console.log(err);
+            // alert(err.response.data.message);
+        }finally{
+            setLoading(false);
+        }
+    };
+
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit,
+    });
+
+    return (
+        <Container className="mt-4 ">
+            <Form noValidate onSubmit={formik.handleSubmit}>
+                <Form.Group className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="First Name"
+                        {...formik.getFieldProps("firstName")}
+                        isInvalid={formik.touched.firstName && !!formik.errors.firstName}
+                        isValid={formik.touched.firstName && !formik.errors.firstName}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.firstName}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Last Name"
+                        {...formik.getFieldProps("lastName")}
+                        isInvalid={formik.touched.lastName && !!formik.errors.lastName}
+                        isValid={formik.touched.lastName && !formik.errors.lastName}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.lastName}
+                    </Form.Control.Feedback>
+                </Form.Group><Form.Group className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        {...formik.getFieldProps("email")}
+                        isInvalid={formik.touched.email && !!formik.errors.email}
+                        isValid={formik.touched.email && !formik.errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.email}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-2" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        {...formik.getFieldProps("password")}
+                        isInvalid={formik.touched.password && !!formik.errors.password}
+                        isValid={formik.touched.password && !formik.errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.password}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Adress"
+                        {...formik.getFieldProps("address")}
+                        isInvalid={formik.touched.address && !!formik.errors.address}
+                        isValid={formik.touched.address && !formik.errors.address}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.address}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>Phone Number</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Phone Number"
+                        {...formik.getFieldProps("phoneNumber")}
+                        isInvalid={formik.touched.phoneNumber && !!formik.errors.phoneNumber}
+                        isValid={formik.touched.phoneNumber && !formik.errors.phoneNumber}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors.phoneNumber}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <div className="w-full h-14 flex items-center justify-end ">
+                    <div>
+                    <button className=" mb-3 h-10  w-24 rounded-lg bg-slate-700 text-slate-100  hover:text-slate-950 hover:bg-gray-100   "  type="submit" 
+                    disabled={!(formik.dirty && formik.isValid) || loading}
+                >
+                    {loading && <Spinner animation="border" size="sm" />}
+                Register
+                    </button>
+                    </div>
+                    
+                </div>
+
+            </Form>
+        </Container>
+
+
+
+    );
+};
+
+export default UpdateUserProfile;

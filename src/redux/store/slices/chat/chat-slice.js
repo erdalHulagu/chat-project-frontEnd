77cc78f7490d0120bbbb55775294272c6edcde4e
erdalHulagu <erdalhulahu@gmail.com>
2024-06-21 20:20:39 +0100

    21/06/24

--- src/redux/store/slices/chat/chat-slice.js
+++ src/redux/store/slices/chat/chat-slice.js
@@ -1,3 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState ={}
+const initialState = {
+    userChats: [],
+    createdGroup: null,
+    createdChat: null,
+}
+
+export const chatSlice = createSlice({
+    name: "chat",
+    initialState,
+
+    reducers: {
+        createChat: (state, action) => {
+            state.createdChat = action.payload
+        },
+
+        createGroup: (state, action) => {
+            state.createdGroup = action.payload
+
+        },
+        chats: (state, action) => {
+            state.userChats = action.payload
+        }
+    }
+});
+export const { createChat, createGroup, chats } = chatSlice.actions;
+export default chatSlice.reducer;
+
+
+
+
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,11 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import profileReducer from './slices/friend/friendsProfileSlices'
 import createdGroupReducer from './slices/group/created-group-slice';
 import authReducer from './slices/auth/auth-slice'
+import chatReducer from './slices/chat/chat-slice'
 export const store = configureStore({
   reducer: {
     profile: profileReducer,
     createGroup:createdGroupReducer,
     auth:authReducer,
+    chat:chatReducer,
+
     
     // other reducers...
   }

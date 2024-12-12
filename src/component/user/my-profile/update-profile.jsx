e40917214583176c8939d805ea69a5057f0dc423
erdalHulagu <erdalhulahu@gmail.com>
2024-12-12 13:38:57 +0000

    12/12/24

--- src/component/user/my-profile/update-profile.jsx
+++ src/component/user/my-profile/update-profile.jsx
@@ -92,132 +92,144 @@ const UpdateProfile = () => {
                 }
 
             </div>
-           
-                <div className=" h-full w-[70%] flex rounded items-center justify-center">
-                    <div className='opacity-80 w-full rounded  max-w-2xl  flex justify-center '>
-{update &&
-                        <Form className="w-[70%]  text-white" noValidate onSubmit={formik.handleSubmit}>
-                            <Form.Group className="mb-5" controlId="formBasicEmail">
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder={user.firstName}
-                                    {...formik.getFieldProps("firstName")}
-                                    isInvalid={formik.touched.firstName && !!formik.errors.firstName}
-                                    isValid={formik.touched.firstName && !formik.errors.firstName}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.firstName}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group className="mb-5" controlId="formBasicEmail">
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder={user.lastName}
-                                    {...formik.getFieldProps("lastName")}
-                                    isInvalid={formik.touched.lastName && !!formik.errors.lastName}
-                                    isValid={formik.touched.lastName && !formik.errors.lastName}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.lastName}
-                                </Form.Control.Feedback>
-                            </Form.Group><Form.Group className="mb-5" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder={user.email}
-                                    {...formik.getFieldProps("email")}
-                                    isInvalid={formik.touched.email && !!formik.errors.email}
-                                    isValid={formik.touched.email && !formik.errors.email}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.email}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group className="mb-5" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="......."
-                                    {...formik.getFieldProps("password")}
-                                    isInvalid={formik.touched.password && !!formik.errors.password}
-                                    isValid={formik.touched.password && !formik.errors.password}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.password}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group className="mb-5" controlId="formBasicEmail">
-                                <Form.Label>Address</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder={user.address}
-                                    {...formik.getFieldProps("address")}
-                                    isInvalid={formik.touched.address && !!formik.errors.address}
-                                    isValid={formik.touched.address && !formik.errors.address}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.address}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group className="mb-5" controlId="formBasicEmail">
-                                <Form.Label>Post Code</Form.Label>
-                                <Form.Control
-                                    type="postCode"
-                                    placeholder={user.postCode}
-                                    {...formik.getFieldProps("postCode")}
-                                    isInvalid={formik.touched.postCode && !!formik.errors.postCode}
-                                    isValid={formik.touched.postCode && !formik.errors.postCode}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.postCode}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group className="mb-5" controlId="formBasicEmail">
-                                <Form.Label>Phone Number</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder={user.phone}
-                                    {...formik.getFieldProps("phoneNumber")}
-                                    isInvalid={formik.touched.phoneNumber && !!formik.errors.phoneNumber}
-                                    isValid={formik.touched.phoneNumber && !formik.errors.phoneNumber}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.phoneNumber}
-                                </Form.Control.Feedback>
-                            </Form.Group>
 
-                            <div className="w-full h-14 flex items-center justify-end ">
-                                <div>
-                                    <button className=" mb-3 h-10  w-24 rounded-lg bg-slate-700 text-slate-100  hover:text-slate-950 hover:bg-gray-100   " type="submit"
-                                        disabled={!(formik.dirty && formik.isValid) || loading}
-                                    >
-                                        {loading && <Spinner animation="border" size="sm" />}
-                                        Submit
-                                    </button>
-                                </div>
+            <div className=" h-full w-[70%] rounded ">
+                <div className='opacity-80 w-full rounded  max-w-2xl  flex justify-center '>
+                    {!update ? 
+                    <div className="h-30 w-full">
+                    
+                        <div onClick={updateProfile} className="h-full w-full flex items-center justify-end ">
+                            <button className="flex  my-3  items-center cursor-pointer justify-evenly h-10  w-48 rounded-lg  hover:text-slate-50 text-gray-300 hover:text-xl" type="submit" ><FaLongArrowAltRight /> Up Date Profile</button>
+                        </div>
 
-                            </div>
 
-                        </Form>
-            }
-
-                    </div>
-                    {!update ?
-                <div onClick={updateProfile} className="h-full w-full flex  justify-end  ">
-                    <button className="mt-3 flex items-center justify-evenly cursor-pointer mb-3 h-10  w-48 rounded-lg  text-slate-100  hover:text-slate-950 hover:bg-gray-100   " type="submit" ><FaLongArrowAltRight /> Up Date Profile</button>
-
+                    
                 </div>
                 :
-                <div onClick={updateProfile} className="h-full w-full  flex  justify-end ">
-                  <button className="my-3 flex items-center cursor-pointer justify-evenly h-10  w-48 rounded-lg  text-slate-100  hover:text-slate-950 hover:bg-gray-100   " type="submit" >  <FaLongArrowAltLeft />Back</button>
+                    <div className="h-full w-full ">
+                        <div onClick={updateProfile} className="h-[10%] w-full flex items-center justify-end  ">
+                            <div className=" flex  my-3  items-center cursor-pointer justify-evenly h-10  w-48 rounded-lg  hover:text-slate-50 text-gray-300 hover:text-xl" type="submit" >  <FaLongArrowAltLeft />Back</div>
+
+                        </div>
+                        <div className="h-[70%] w-full flex items-center justify-center">
+
+
+                            <Form className="w-[70%]  text-white" noValidate onSubmit={formik.handleSubmit}>
+                                <Form.Group className="mb-5" controlId="formBasicEmail">
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder={user.firstName}
+                                        {...formik.getFieldProps("firstName")}
+                                        isInvalid={formik.touched.firstName && !!formik.errors.firstName}
+                                        isValid={formik.touched.firstName && !formik.errors.firstName}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.firstName}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-5" controlId="formBasicEmail">
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder={user.lastName}
+                                        {...formik.getFieldProps("lastName")}
+                                        isInvalid={formik.touched.lastName && !!formik.errors.lastName}
+                                        isValid={formik.touched.lastName && !formik.errors.lastName}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.lastName}
+                                    </Form.Control.Feedback>
+                                </Form.Group><Form.Group className="mb-5" controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder={user.email}
+                                        {...formik.getFieldProps("email")}
+                                        isInvalid={formik.touched.email && !!formik.errors.email}
+                                        isValid={formik.touched.email && !formik.errors.email}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.email}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-5" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="......."
+                                        {...formik.getFieldProps("password")}
+                                        isInvalid={formik.touched.password && !!formik.errors.password}
+                                        isValid={formik.touched.password && !formik.errors.password}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.password}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-5" controlId="formBasicEmail">
+                                    <Form.Label>Address</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder={user.address}
+                                        {...formik.getFieldProps("address")}
+                                        isInvalid={formik.touched.address && !!formik.errors.address}
+                                        isValid={formik.touched.address && !formik.errors.address}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.address}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-5" controlId="formBasicEmail">
+                                    <Form.Label>Post Code</Form.Label>
+                                    <Form.Control
+                                        type="postCode"
+                                        placeholder={user.postCode}
+                                        {...formik.getFieldProps("postCode")}
+                                        isInvalid={formik.touched.postCode && !!formik.errors.postCode}
+                                        isValid={formik.touched.postCode && !formik.errors.postCode}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.postCode}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-5" controlId="formBasicEmail">
+                                    <Form.Label>Phone Number</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder={user.phone}
+                                        {...formik.getFieldProps("phoneNumber")}
+                                        isInvalid={formik.touched.phoneNumber && !!formik.errors.phoneNumber}
+                                        isValid={formik.touched.phoneNumber && !formik.errors.phoneNumber}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.phoneNumber}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+
+                                <div className="w-full h-14 flex items-center justify-end ">
+                                    <div>
+                                        <button className=" mb-3 h-10  w-24 rounded-lg bg-slate-700 text-slate-100  hover:text-slate-950 hover:bg-gray-100   " type="submit"
+                                            disabled={!(formik.dirty && formik.isValid) || loading}
+                                        >
+                                            {loading && <Spinner animation="border" size="sm" />}
+                                            Submit
+                                        </button>
+                                    </div>
+
+                                </div>
+
+                            </Form>
+                        </div>
+                    </div>
+                    }
 
                 </div>
-            }
-                </div>
-            
-            
+
+
+
+            </div>
+
+
         </div>
 
     );
